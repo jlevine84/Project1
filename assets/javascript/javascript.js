bb22fd896b74458af4d4c44adcf142c7d5b198c6
Emma Dedmond <emmadedmond@yahoo.com>
2019-02-02 12:16:06 -0500

    changed to NEWS API and added favorite heart (but not functionality)

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,8 +1,11 @@
 var idCount=1;
-var slice = "/12351/"
+var slice = "/12351/";
+var articleSection = $("#log");
+var articles = [];
 var yandexKey = "trnsl.1.1.20190130T012434Z.3dd2c347532d5fa2.316531bda0cbd1d627d27d686ed25ff9b2b799d7";
 var translateURL = "https://translate.yandex.net/api/v1.5/tr.json/translate?";
 translateURL = translateURL + "key=" + yandexKey;
+var lang = "en";
 // lang = "en" to translate to english
 // text = "text" DONT ACTUALLY NEED QUOTES
 // callback= callbackFunction
@@ -16,65 +19,55 @@ function translate (language, text) {
         $("#"+answer[0]).text(answer[1]);
     })
 }
-//New York Times API
-var apiKey = "2uLSCfNAfY93MR1jQWuIUkNjuKASxxGD";
-var articleSection = $("#log");
-articleSection.html("<p>This should show up</p>");
-function search1(term,language) {
-    console.log("first language : " + language);
-    search(term,10,2018,2019,language);
-}
-function search(term,limit,startYear,endYear,lang) {
-    console.log("language = " +lang);
-    var queryURL = "https://cors-anywhere.herokuapp.com/https://api.nytimes.com/svc/search/v2/articlesearch.json?"
-    var dates = ""; 
-    if (startYear < endYear) {
-        dates= "&begin_date="+startYear+"0101&end_date=" +endYear+"1231";
-    }
-    queryURL = queryURL + dates
-    queryURL = queryURL + "&q="+term+"&sort=oldest&api-key=" +apiKey;
-    var loading = $("<img>").attr("src","https://media0.giphy.com/media/sSgvbe1m3n93G/200.gif");
-    loading.addClass("loadingGif");
-    // console.log(queryURL);
-    $("body").append(loading);
-    $.get(queryURL).then(function(response) {
-        loading.remove();
-        console.log(response);
-        // console.log(response.response.docs[0].source);
-        if (response.status !== "OK") {
-            return false;
-        }
-        articleSection.html("");
-        var data = response.response.docs;
-        console.log(data.length);
-        if (data.length < 1) {
-            articleSection.html("<p>No articles found</p>")
-            return false;
-        }
-        for (var i = 0; i < data.length;i++) {
-            if (i >= limit) {
-                break;
-            }
-            var article = $("<div>");
-            article.addClass("article");
-            var title = $("<h6>").attr("id",idCount);
-            title.addClass("title");
-            translate(lang,idCount + slice +data[i].headline.main);
-            idCount++;
-            var snip = $("<p>").attr("id",idCount);
-            snip.addClass("snippet");
-            translate(lang,idCount + slice +data[i].snippet);
-            idCount++;
-            var url = $("<a>").text("read more").attr("href",data[i].web_url)
-            url.addClass("articleLink");
-            var dateString = data[i].pub_date.split("T")[0];
-            var date = $("<p>").text(dateString);
-            date.addClass("date");
-            article.append(title,snip,date,url);
-            articleSection.append(article);
-        }
-    })
-}
+var newsKey = "&apiKey=bad2768f143a4bd39c2c92889b981643";
+var newsURL = "https://newsapi.org/v2/everything?"
 
-search1("trump","fr");
+function translateArticles() {
+    console.log("got here");
+    if (articles.length < 1) {
+        articleSection.html("<p>No articles found</p>")
+        return false;
+    }
+    limit = 10;
+    for (var i = 0; i < articles.length;i++) {
+        if (i >= limit) {
+            break;
+        }
+        var article = $("<div>");
+        article.addClass("article");
+        var title = $("<h6>").attr("id",idCount);
+        title.addClass("title");
+        translate(lang,idCount + slice + articles[i].title);
+        idCount++;
+        var snip = $("<p>").attr("id",idCount);
+        snip.addClass("snippet");
+        translate(lang,idCount + slice +articles[i].description);
+        idCount++;
+        var url = $("<a>").text("read more").attr("href",articles[i].url)
+        url.addClass("articleLink");
+        var dateString = articles[i].publishedAt.split("T")[0];
+        var date = $("<p>").text(dateString);
+        date.addClass("date");
+         var favorite = $("<i>");
+        favorite.attr("id",i);
+        favorite.addClass("far fa-heart")
+        article.append(favorite,title,snip,date,url);
+        articleSection.append(article);
+    }
+}
+function search(term,startDate,sources,sortBy) {
+    var URL = newsURL + "q=" + term + "&from=" + startDate + "&sources=" + sources +"&sortBy=" + sortBy;
+    URL = URL + newsKey;
+    $.get(URL).then(function(response) {
+        console.log(response);
+        if (response.status !== "ok") {
+            return false;
+        }
+        articles = response.articles;
+        translateArticles();
+    });
+}
+search("trump","2019-01-14","","publishedAt")
+
+
 
